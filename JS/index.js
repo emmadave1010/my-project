4e62c2ce5b0bacc25719db0de4d3a127b66f4bbe
emmanuel onyekwere <emmadino013@gmail.com>
2026-05-24 10:26:55 +0100

    I created another file call responsive

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.hamburger')
+const navLinks = document.querySelector('.nav-links')
+const links = document.querySelectorAll('.nav-links a')
+
+
+hamburger.addEventListener('click', ()=>{
+    navLinks.classList.toggle('active')
+    hamburger.classList.toggle('active')
+})
+
+links.forEach((link, index)=>{
+    link.addEventListener('click', ()=>{
+        navLinks.classList.remove('active')
+        hamburger.classList.remove('active')
+    })
+})
